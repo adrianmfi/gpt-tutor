08a1926b1784868af4107de789fcfda89ff9da42
Adrian Meidell Fiorito <adrian@sparkel.io>
2023-10-12 10:46:53 +0200

    Remove prosody and break tags as they break azure

--- gpt-tutor-lib/src/create-transcript.ts
+++ gpt-tutor-lib/src/create-transcript.ts
@@ -71,7 +71,7 @@ Keep "fluff" to a minimum. Start by saying "Welcome to Adrian's AI lessons. This
 End the lesson by summarizing.
 "This lesson we've learned..."
 
-Remember that a lesson typically contains repetition and pauses, and is spoken slower than a regular conversation, so include this in the output. 
+Remember that a lesson typically contains repetition and pauses.
 When learning a new word, it is helpful to include a sentence including the word.
 A word and sentence should typically be repeated several times, repetition is key to learning. Don't be terse, lessons should last for a while.
 
@@ -86,41 +86,33 @@ This is better:
 The SSML should begin with:
 \`<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="https://www.w3.org/2001/mstts" xml:lang="string">\`
 
-You can use the \`prosody\` tag to adjust pronunciation and speaking rate. A good default amount might be -10%.
-
 Here are the supported languages, you can only use one of these: ar-EG, ar-SA, ca-ES, cs-CZ, da-DK, de-AT, de-CH, de-DE, en-AU, en-CA, en-GB, en-HK, en-IE, en-IN, en-US, es-ES, es-MX, fi-FI, fr-BE, fr-CA, fr-CH, fr-FR, hi-IN, hu-HU, id-ID, it-IT, ja-JP, ko-KR, nb-NO, nl-BE, nl-NL, pl-PL, pt-BR, pt-PT, ru-RU, sv-SE, th-TH, tr-TR, zh-CN, zh-HK, zh-TW
 
-An simple example of an SSML document:
-<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="https://www.w3.org/2001/mstts" xml:lang="en-US">
-<voice name="${voiceName}">
-  <prosody rate="-10%">
-      <lang xml:lang="en-US">
-        Welcome to Adrian's AI lessons. This lesson will talk about the Japanese verbs "to eat" and "to drink".
-      </lang>
-      <break time="1s"/>
-      <lang xml:lang="en-US">
-        Let's start with the verb "to eat", which in Japanese is 
-      </lang>
-    </prosody>
-    <prosody rate="-20%">
-      <lang xml:lang="ja-JP">
-        食べます
-      </lang>
-    </prosody>
-    <prosody rate="-10%">
-      <break time="1s"/>
-      <lang xml:lang="en-US">
-          That's 
-      </lang>
-      <lang xml:lang="ja-JP">食べます</lang>
-    </prosody>
-  </voice>
+Remember to add breaks between words and sentences. Add breaks between sentences by using periods. Add breaks between repetitions in the same language with line breaks. 
+An simple example:
+<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis"
+    xmlns:mstts="https://www.w3.org/2001/mstts" xml:lang="en-US">
+    <voice name="en-US-RyanMultilingualNeural">
+        <lang xml:lang="en-US">
+            Welcome to Adrian's AI lessons. This lesson will talk about the Japanese verbs "to eat" and "to drink".
+            First, let's learn the phrase for "please". In Japanese, we say:
+        </lang>
+        <lang xml:lang="ja-JP">お願いします</lang>
+        <lang xml:lang="en-US">. That's</lang>
+        <lang xml:lang="ja-JP">お願いします</lang>
+        <lang xml:lang="en-US">. Let's repeat it:</lang>
+        <lang xml:lang="ja-JP">
+            お願いします
+
+            お願いします
+        </lang>
+        <lang xml:lang="en-US">. Great job!</lang>
+    </voice>
 </speak>
 
 Your transcript should be much longer than the simple example.
 
-Remember to repeat new words up to several times! Also, remember say new words extra slow, especially the first time(s) they are introduced! Finally, remember to NOT use foreign and english words within the same <lang>.
- 
+Remember to repeat new words up to several times! Finally, remember to NOT use foreign and english words within the same <lang>.
 
 Now, the lesson description is:
 The lesson is a part of a series for learning ${goals.targetLanguage}.
