3d1fae8f09b43c5f7ed621a34ffcb29dd792485e
Adrian Meidell Fiorito <adrian@sparkel.io>
2023-10-22 22:31:12 +0200

    Update prompt to say "which translates to" less

--- src/lib/create-transcript.ts
+++ src/lib/create-transcript.ts
@@ -143,7 +143,7 @@ function createSystemPrompt(
 
   const supportedLanguages =
     "ar-EG, ar-SA, ca-ES, cs-CZ, da-DK, de-AT, de-CH, de-DE, en-AU, en-CA, en-GB, en-HK, en-IE, en-IN, en-US, es-ES, es-MX, fi-FI, fr-BE, fr-CA, fr-CH, fr-FR, hi-IN, hu-HU, id-ID, it-IT, ja-JP, ko-KR, nb-NO, nl-BE, nl-NL, pl-PL, pt-BR, pt-PT, ru-RU, sv-SE, th-TH, tr-TR, zh-CN, zh-HK, zh-TW";
-  return `You are a bot tasked with generating a transcript personalized audio lesson.
+  return `You are a pedagogic tutor bot tasked with generating a transcript for personalized audio lesson.
 Your transcript will be converted to audio by a text-to-speech API without human intervention.
 The lesson is a part of a series of lessons for a user learning a specified language.
 The base language is English, and the lesson will incorporate both English and a target language.
@@ -157,26 +157,26 @@ An audio lesson could for example include:
 The transcript should read as a regular text, with one notable exception: enclose any foreign words in <lang lang="name-of-language"></lang> tags to ensure correct pronunciation. This is vital for text-to-speech to work.
 
 Here are some shortened examples:
-* Example 1 - Words for directions:
-Let's go through the words for 'left' and 'right'.
+* Example 1 beginner level - Words for directions:
+Let's go through the words for left and right.
 
-First up is the word for 'left', which in Japanese is: <lang lang="ja-JP">左</lang>.
-Once more, 'left' is: <lang lang="ja-JP">左</lang>.
-You could for example say "Go left" which translates to: <lang lang="ja-JP">左に行って下さい</lang>.
-Again, 'Go left' is: <lang lang="ja-JP">左に行って下さい</lang>.
+First up is the word for left, which in Japanese is: <lang lang="ja-JP">左</lang>.
+Once more, left is: <lang lang="ja-JP">左</lang>.
+You could for example say: "It's on the left", which translates to: <lang lang="ja-JP">左です</lang>.
+Again, "It's on the left" is: <lang lang="ja-JP">左です</lang>.
 
-Now we will learn the word 'right'. In Japanese, right is: <lang lang="ja-JP">右</lang>.
-Again, 'right' is: <lang lang="ja-JP">右</lang>.
-A useful phrase would be: "Turn right", which translates to: <lang lang="ja-JP">右に曲がって下さい</lang>.
-And again, 'Turn right' is: <lang lang="ja-JP">右に曲がって下さい</lang>.
+Now we will learn the word right. Right is: <lang lang="ja-JP">右</lang>.
+Again, right is: <lang lang="ja-JP">右</lang>.
+To say "It's on the right", you can say: <lang lang="ja-JP">右です</lang>.
+Repeating, to say "It's on the right": <lang lang="ja-JP">右です</lang>.
 
-We have now learned how to say 'left' as <lang lang="ja-JP">左</lang>, 'right' as <lang lang="ja-JP">右</lang>
+We have now learned how to say left as <lang lang="ja-JP">左</lang> and right as <lang lang="ja-JP">右</lang>.
 
-* Example 2 - Conversation at the Restaurant:
+* Example 2 - beginner level - Conversation at the Restaurant:
 We will now practice a conversation in a Norwegian restaurant setting.
 Imagine you're at a restaurant in Oslo. You're about to order your meal.
 
-First up, to catch the waiter's attention with "excuse me", the phrase in Norwegian is <lang lang="nb-NO">Unnskyld</lang>.
+First up, to catch the waiter's attention with "excuse me", the phrase in Norwegian is: <lang lang="nb-NO">Unnskyld</lang>.
 Once more: <lang lang="nb-NO">Unnskyld</lang>.
 
 Next, to request something with "may I have", use <lang lang="nb-NO">Kan jeg få</lang>.
@@ -185,7 +185,7 @@ Go ahead and practice: <lang lang="nb-NO">Kan jeg få</lang>.
 Combine these to ask for the menu: <lang lang="nb-NO">Unnskyld, kan jeg få menyen?</lang>.
 Again: <lang lang="nb-NO">Unnskyld, kan jeg få menyen?</lang>.
 
-To which the waiter might respond "Here you go", or in Norwegian <lang lang="nb-NO">Vær så god</lang>.
+To which the waiter might respond "Here you go", or in Norwegian: <lang lang="nb-NO">Vær så god</lang>.
 Repeating the waiter's response: <lang lang="nb-NO">Vær så god</lang>.
 
 This concludes the conversation at the restaurant.
